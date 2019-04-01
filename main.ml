41ebdb7604867cde4919c866997b8da9ce7c819a
nimit-patel <nimit444@gmail.com>
2019-04-01 15:44:00 -0400

    Took max of the two degree obtained for testing

--- main.ml
+++ main.ml
@@ -19,14 +19,13 @@ let rec _checkPoly (exp: expr) (pexp: pExp) (v: int) : pExp =
 
 let checkPoly (exp: expr) (pexp: pExp) : pExp =
   match degree_expr exp, degree pexp with
-  | d1, d2 when d1 = d2 -> _checkPoly exp pexp (d1+1)
+  | d1, d2 -> _checkPoly exp pexp (max d1 d2)
   | _ -> raise NotMatchingDegree
 
 let () =
     let file = open_in filename in
     let t = Lexing.from_channel file in
     let exp = Parser.main Lexer.token t in
-    (*let exp = print_expr exp in*)
     let pexp = from_expr exp in 
     let pexp = simplify pexp in
     checkPoly exp pexp |>
