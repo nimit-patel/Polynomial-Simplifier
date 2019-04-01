18b8f358d845a99246313eddabdfe6be9e30e3b3
dacabdi <dacabdi89@ufl.edu>
2019-04-01 18:43:29 -0400

    fix argument order in List.stable_sort

--- poly.ml
+++ poly.ml
@@ -1,4 +1,5 @@
 open Core
+open Expr
 
 exception Unrecognized_pExpr
 exception InvalidArgumentsForDistributive of string
@@ -131,7 +132,7 @@ let rec simplify1 (e:pExp): pExp =
     match l with 
     | l::[] -> l
     | _ -> (
-      List.stable_sort l compareDeg         |>
+      List.stable_sort compareDeg l         |>
       List.fold ~init:[] ~f:flatPlus        |>
       List.fold ~init:[] ~f:accumulatePlus  |>
       Plus
@@ -141,7 +142,7 @@ let rec simplify1 (e:pExp): pExp =
     match l with 
     | l::[] -> l
     | _ -> (
-      List.stable_sort l compareDeg         |>
+      List.stable_sort compareDeg l         |>
       List.fold ~init:[] ~f:flatTimes       |>
       List.fold ~init:[] ~f:accumulateTimes |>
       List.fold ~init:[] ~f:distribute      |>
