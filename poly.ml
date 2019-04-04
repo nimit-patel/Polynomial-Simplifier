2ed9f8e0fd69d1c6cee20dd649bfc801cba06d58
David Cabrera <dacabdi89@ufl.edu>
2019-04-04 13:35:27 -0400

    sorting different versions of the Core package of OCaml take in the arguments for sorting in a different order

--- poly.ml
+++ poly.ml
@@ -128,7 +128,7 @@ let rec simplify1 (e:pExp): pExp =
     match l with 
     | l::[] -> l
     | _ -> (
-      List.stable_sort compareDeg l         |>
+      List.stable_sort l compareDeg         |>
       List.fold ~init:[] ~f:flatPlus        |>
       List.fold ~init:[] ~f:accumulatePlus  |>
       Plus
@@ -138,7 +138,7 @@ let rec simplify1 (e:pExp): pExp =
     match l with 
     | l::[] -> l
     | _ -> (
-      List.stable_sort compareDeg l         |>
+      List.stable_sort l compareDeg         |>
       List.fold ~init:[] ~f:flatTimes       |>
       List.fold ~init:[] ~f:accumulateTimes |>
       List.fold ~init:[] ~f:distribute      |>
